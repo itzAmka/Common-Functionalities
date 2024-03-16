f420585c98df3167bcfb9cfe158671985286d650
Amin <Aminhassen85@gmail.com>
2024-03-16 16:47:07 -0400

    add products page

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<svelte:head>
+	<title>Products | Filtering</title>
+</svelte:head>
+
+<h1 class="text-3xl font-bold underline">Products Page!</h1>
