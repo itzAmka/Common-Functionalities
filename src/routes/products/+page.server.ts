18aaa0e1f81a4605e21a96da5408939797b19fdf
Amin <Aminhassen85@gmail.com>
2024-03-16 16:50:46 -0400

    load products from fake store api

--- src/routes/products/+page.server.ts
+++ src/routes/products/+page.server.ts
@@ -1,7 +1,16 @@
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
-export const load = (async () => {
+export const load = (async ({ fetch }) => {
+	const res = await fetch('https://fakestoreapi.com/products');
+
+	if (!res.ok) {
+		error(res.status, 'Failed to load products');
+	}
+
+	const products = await res.json();
+
 	return {
-		products: []
+		products
 	};
 }) satisfies PageServerLoad;
