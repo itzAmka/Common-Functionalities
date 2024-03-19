72b255f1b0f5aa46f41efbc17b4d614f6afb952b
Amin <Aminhassen85@gmail.com>
2024-03-19 16:22:55 -0400

    add sorting functionality page

---
+++ src/routes/sorting/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<svelte:head>
+	<title>Sorting Functionality</title>
+</svelte:head>
+
+<section>
+	<div class="container lg:px-20 md:px-10 px-6 py-20 flex flex-col gap-20">
+		<section class="prose">
+			<h1 class="text-primary mb-0">Sorting Funtionality</h1>
+			<p>Use the Sorting functionality to sort your data.</p>
+
+			<!-- links -->
+			<div class="flex gap-4 w-fit">
+				<a href="/" class="btn btn-sm btn-accent">/Home</a>
+				<a href="/filtering" class="btn btn-sm btn-accent">/Filtering</a>
+				<a href="/searching" class="btn btn-sm btn-accent">/Searching</a>
+			</div>
+		</section>
+	</div>
+</section>
