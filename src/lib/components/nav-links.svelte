d90d8c9c97615b511176e43bf6db89a7a60873cc
Amin <Aminhassen85@gmail.com>
2024-03-19 16:32:04 -0400

    add navigation links component

---
+++ src/lib/components/nav-links.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex gap-4 w-fit">
+	<a href="/" class="btn btn-sm {$page.url.pathname === '/' ? 'btn-primary' : 'btn-accent'}">
+		/Home
+	</a>
+	<a
+		href="/filtering"
+		class="btn btn-sm {$page.url.pathname === '/filtering' ? 'btn-primary' : 'btn-accent'}"
+	>
+		/Filtering
+	</a>
+	<a
+		href="/sorting"
+		class="btn btn-sm {$page.url.pathname === '/sorting' ? 'btn-primary' : 'btn-accent'}"
+	>
+		/Sorting
+	</a>
+	<a
+		href="/searching"
+		class="btn btn-sm {$page.url.pathname === '/searching' ? 'btn-primary' : 'btn-accent'}"
+	>
+		/Searching
+	</a>
+</div>
