95f99bdd09cf07036924b45eb24022876d01a5c0
Amin <Aminhassen85@gmail.com>
2024-03-19 16:07:08 -0400

    update page layout and links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,21 @@
 	<title>Home | Filtering</title>
 </svelte:head>
 
-<h1 class="text-3xl font-bold underline">Filtering Project!</h1>
+<section class="flex items-center justify-center min-h-screen p-4">
+	<div
+		class="border flex flex-col gap-10 shadow-lg rounded-lg p-6 py-10 border-success shadow-black"
+	>
+		<div class="prose">
+			<h1 class="text-3xl font-bold">Common Funtionality</h1>
+			<p>
+				Here are some common functionality that you can use to filter, search, and sort your data.
+			</p>
+		</div>
+
+		<div class="flex gap-4 w-fit">
+			<a href="/products" class="btn btn-sm btn-primary">/Filtering</a>
+			<a href="/products" class="btn btn-sm btn-primary">/Searching</a>
+			<a href="/products" class="btn btn-sm btn-primary">/Sorting</a>
+		</div>
+	</div>
+</section>
