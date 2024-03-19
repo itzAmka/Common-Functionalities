a37e7a56565deb343a63664daa0094c0bf2e213d
Amin <Aminhassen85@gmail.com>
2024-03-19 16:22:18 -0400

    add link to other pages

--- src/routes/filtering/+page.svelte
+++ src/routes/filtering/+page.svelte
@@ -74,8 +74,15 @@
 <section>
 	<div class="container lg:px-20 md:px-10 px-6 py-20 flex flex-col gap-20">
 		<section class="prose">
-			<h1 class="text-primary mb-0">Products List</h1>
+			<h1 class="text-primary mb-0">Filtering Functionality</h1>
 			<p>Use the filters to find the product you are looking for.</p>
+
+			<!-- links -->
+			<div class="flex gap-4 w-fit">
+				<a href="/" class="btn btn-sm btn-accent">/Home</a>
+				<a href="/sorting" class="btn btn-sm btn-accent">/Sorting</a>
+				<a href="/searching" class="btn btn-sm btn-accent">/Searching</a>
+			</div>
 		</section>
 
 		<section class="flex flex-col lg:gap-20 gap-10 relative">
