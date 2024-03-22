4e2c27e8394f319c9583f4682d8d505b2ddc324e
Amin <Aminhassen85@gmail.com>
2024-03-21 23:13:37 -0400

    add pagination functionality

--- src/routes/searching/+page.server.ts
+++ src/routes/searching/+page.server.ts
@@ -2,8 +2,14 @@ import type { PageServerLoad } from './$types';
 import type { TUser } from '$lib/types/users';
 import { error } from '@sveltejs/kit';
 
-export const load = (async ({ fetch }) => {
-	const usersRes = await fetch('https://dummyjson.com/users?limit=10');
+export const load = (async ({ fetch, url }) => {
+	let pageNumber = Number(url.searchParams.get('pageNumber') || '1');
+	const limit = 10;
+	const skip = (pageNumber - 1) * limit;
+
+	const usersRes = await fetch(
+		`https://dummyjson.com/users?limit=${limit}&skip=${skip}&page=${pageNumber}`
+	);
 
 	if (!usersRes.ok) {
 		error(usersRes.status, 'Failed to load products');
@@ -11,8 +17,18 @@ export const load = (async ({ fetch }) => {
 
 	const usersData = await usersRes.json();
 
+	const totalCount = usersData.total;
+	pageNumber = Math.min(Math.ceil(totalCount / limit), pageNumber);
+	const totalPages = Math.ceil(totalCount / limit);
+
 	return {
 		users: usersData.users as TUser[],
-		pagination: {}
+		pagination: {
+			totalCount,
+			skip,
+			pageNumber,
+			limit,
+			totalPages
+		}
 	};
 }) satisfies PageServerLoad;
